ccfc560172b1ba5646ce450f0d732eb251a485c7
Jim Cramer <remarcmij@gmail.com>
2023-03-03 21:28:19 +0100

    Use console.table for the dump

--- dump.js
+++ dump.js
@@ -6,6 +6,11 @@ function main() {
   const hyf = HyfOrganization.createSeeded();
   console.table(hyf.classes);
 
+  hyf.classes.forEach((cls) => {
+    console.log(`\nStudents of ${cls.name}:`);
+    console.table(cls.students);
+  });
+
   // const students = hyf.findStudentsByName('satter');
   // console.log(util.inspect(hyf, null, Infinity));
 }
